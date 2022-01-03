3b68174f90e2abe81abc98d69065aa0bd4b14cd7
Kairolite <christo.erwin@gmail.com>
2022-01-03 20:25:39 +0700

    First commit, hello world program.

--- 01HelloWorld/Program.cs
+++ 01HelloWorld/Program.cs
@@ -4,9 +4,13 @@ namespace _01HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        // Entry point of our program. This is a comment.
+        static void Main(string[] args) //static, void, method
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Chris!");
+            // in C# semicolon is important
+            Console.Read();
         }
     }
 }
